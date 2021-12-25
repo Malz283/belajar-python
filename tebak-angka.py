12fc77f05dd316504e3aba036e5b79ec05d0c3e6
Malz283 <leoraf283@gmail.com>
2021-12-25 07:52:30 +0700

    first commit

---
+++ tebak-angka.py
@@ -0,0 +1,15 @@
+import random
+
+def tebak(x):
+    angka_acak = random.randint(1, x)
+    tebak = 0
+    while tebak != angka_acak:
+        tebak = int(input(f'Tebak angka antara 1 sampai {x}: '))
+        if tebak < angka_acak:
+            print('Maaf, coba tebak lagi, terlalu rendah.')
+        elif tebak > angka_acak:
+            print('Maaf, coba tebak lagi, terlalu tinggi.')
+
+    print(f'Wow, selamat. angka {angka_acak} adalah tebakan tepat!')
+
+tebak(1000)
